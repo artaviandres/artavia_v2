93f0001724b0c5f77b2995ad577eecfd8b156482
Andrés Artavia <artaviandres@gmail.com>
2018-01-30 14:40:37 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Andres Test Heroku</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div>
+        <div className="salute">
+            <p>
+                HEY!
+                <br />
+                HOLA!
+                <br />
+                BONJOUR!
+                <br />
+                OLÁ!
+                <br />
+                你好!
+                <br />
+                HALLO!
+            </p>
+        </div>
+        <div className="container">
+            {/* <Menu activePage={1} /> */}
+            <p>
+                Andrés Artavia is a Web Developer based in Costa Rica who is IN LOVE with React
+            </p>
+            {/* <Social /> */}
+        </div>
+        <style jsx>{`
+            body {
+                background-color: #F8F8F8;
+                overflow: hidden;
+            }
+            .container {
+                width: 55%;
+                float: left;
+                background-color: white;
+                height: 100vh;
+            }
+            .container p {
+                margin: 0 auto;
+            }
+            .salute {
+                width: 45%;
+                float: left;
+            }
+            .salute p {
+                font-family: arial;
+                font-size: 120px;
+                margin-top: -50px;
+            }
+        `}</style>
+    </div>
     );
   }
 }
